881a6c8463ded127ff5684c1c413a32242c7bae3
Sandip2491 <patil.sandip2490@gmail.com>
2025-10-02 20:36:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/React-project/", // 👈 must match your GitHub repo name
   plugins: [react()],
 })
